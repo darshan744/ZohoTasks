ff9fa5a963e223378286389af2cfe1a0566312d7
darshan744 <darshankumaravelk@gmail.com>
2025-09-12 18:14:18 +0530

    Added slave failover recovery.

--- Task-2/monitor.sh
+++ Task-2/monitor.sh
@@ -20,7 +20,7 @@ fi
 pg_isready=$binaryDirectory/pg_isready
 pg_ctl=$binaryDirectory/pg_ctl
 pg_basebackup=$binaryDirectory/pg_basebackup
-
+psql=$binaryDirectory/psql
 
 if [ ! -f $pg_isready ];then
     echo "pg_isready doesn't exist"
@@ -60,6 +60,29 @@ master_down() {
 }
 
 
+
+standby_down() {
+    
+    if [ -d $currentEmptyDirectory ];then
+        rm -rf $currentEmptyDirectory;
+    fi
+    
+    $psql --port=$currentMasterPort postgres -c "select pg_drop_replication_slot('replica_slot')"
+
+    $pg_basebackup -h localhost -U repuser --checkpoint=fast -D $currentEmptyDirectory -R --slot=replica_slot -C --port=$currentMasterPort
+
+    local port=$currentStandbyPort;
+    currentStandbyPort=$currentFreePort;
+    currentFreePort=$port;
+
+    local dir=$currentStandbyDirecotry;
+    currentStandbyDirecotry=$currentEmptyDirectory
+    currentEmptyDirectory=$currentStandbyDirecotry
+
+    $pg_ctl -D $currentStandbyDirecotry -o "-p $currentStandbyPort" start
+}
+
+
 monitor(){
 
     while true;do
@@ -76,6 +99,7 @@ monitor(){
         $pg_isready --port=$currentStandbyPort -d postgres >> /dev/null 2>&1;
         if [ $? -ne 0 ];then
             echo "$(date) - [ERROR] Slave server is down"
+            standby_down
         else 
             echo "No problem in slave server" >> slave.log
         fi
