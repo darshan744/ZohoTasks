aa7ed01395ed8e5aefbc3d6e603b80fa71ac80d7
darshan744 <darshankumaravelk@gmail.com>
2025-09-15 15:41:30 +0530

    work in progress for must specify password

--- Task-2/replica.sh
+++ Task-2/replica.sh
@@ -9,6 +9,9 @@ slavePort=3001
 repuser=repuser
 loginUser=darshan
 loginPassword=12345
+superUser=darshank
+superUserPassword=123456789
+
 removeAndCreate() {
     if [ -d $1 ];then
         rm -rf $1
@@ -30,7 +33,7 @@ psql=$binaryDirectory/psql
 pg_backup=$binaryDirectory/pg_basebackup
 
 echo "Initializing master Directory : $masterDataDirectory"
-if ! $initdb -D $masterDataDirectory >> replica.log 2>&1;then 
+if ! $initdb -D $masterDataDirectory -A md5 --username=$superUser --pwfile=password.txt >> replica.log 2>&1;then 
     echo "Init db failed"
     exit 1
 fi
@@ -41,12 +44,12 @@ if ! $pg_ctl -D $masterDataDirectory -o "-p $masterPort" start >> replica.log 2>
     exit 1
 fi
 echo "Creating login user"
-if ! $psql --port=$masterPort -c "create user $loginUser with replication password '$loginPassword' createrole" $databaseName >> replica.log 2>&1;then
+if ! PGPASSWORD=$(cat $(pwd)/password.txt) $psql --port=$masterPort -U $superUser -c "create user $loginUser with replication password '$loginPassword' createrole" $databaseName >> replica.log 2>&1;then
     echo "Creation of login user failed"
     exit 1;
 fi
-
-if ! $psql --port=$masterPort -U $loginUser -c "CREATE USER $repuser replication" $databaseName >> replica.log 2>&1;then
+echo "Creating replica user"
+if ! PGPASSWORD=$loginPassword $psql --port=$masterPort -U $loginUser -c "CREATE USER $repuser replication" $databaseName >> replica.log 2>&1;then
     echo "Create user failed"
     exit 1
 fi
@@ -69,7 +72,7 @@ echo "Master server running in port $masterPort"
 # --port -> the data incoming port
 
 echo "Configuring standby server"
-if ! $pg_backup -h localhost -U $repuser --checkpoint=fast -D $slaveDataDirectory -R --slot=replica_slot -C --port=$masterPort >> replica.log 2>&1;then
+if !  $pg_backup -h localhost -U $repuser --checkpoint=fast -D $slaveDataDirectory -R --slot=replica_slot -C --port=$masterPort >> replica.log 2>&1;then
     echo "Running pg_backup failed"
     exit 1;
 fi
