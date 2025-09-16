e6f64792550b3f1ad07dba2a8e037fa516a895ad
darshan744 <darshankumaravelk@gmail.com>
2025-09-16 10:03:49 +0530

    split some functions

--- Task-3/create_server.sh
+++ Task-3/create_server.sh
@@ -15,28 +15,30 @@ masterPort=3000
 baseReplicaDirectoryString=$HOME/Documents/Auto/Node
 slavePort=3001
 
-# if source not found clone it
-if [ ! -d $postgres_source_directory ];then
-    echo "Postgres source directory not found"
-    echo "Cloning from github"
-    git clone https://github.com/postgres/postgres
-fi
-
-if [ ! -d $buildDirectory ];then
-    echo "Build directory not found creating directory at location : $buildDirectory"
-fi
-
 version=$1
 numberOfReplicas=$2
 tags=($(git -C $postgres_source_directory tag))
 
+# executable files needed for setting up
+initdb=$buildDirectory/$version/bin/initdb
+pg_ctl=$buildDirectory/$version/bin/pg_ctl
+psql=$buildDirectory/$version/bin/psql
+pg_backup=$buildDirectory/$version/bin/pg_basebackup
 
-if [ $# -ne 2 ];then
-    echo "Illegal number of arguments"
-    echo "[USAGE] ./compile.sh <version-tag> <number-of-replicas>" 
-    exit
-fi
 
+
+# if source not found clone it
+setup_environment() {
+    if [ ! -d $postgres_source_directory ];then
+        echo "Postgres source directory not found"
+        echo "Cloning from github"
+        git clone https://github.com/postgres/postgres
+    fi
+
+    if [ ! -d $buildDirectory ];then
+        echo "Build directory not found creating directory at location : $buildDirectory"
+    fi
+}
 
 find_if_tag_exist() {
     found=0
@@ -53,9 +55,23 @@ find_if_tag_exist() {
     fi
 }
 
+check_input() {
+    if [ $# -ne 2 ];then
+        echo "Illegal number of arguments"
+        echo "[USAGE] ./create_server.sh <version-tag> <number-of-replicas>" 
+        exit 1
+    fi
+
+    if printf -- '%d' "$numberOfReplicas" > /dev/null 2>&1;then
+        echo "Please enter a valid number of replicas"
+        exit 1;
+    fi
+
+    find_if_tag_exist
+}
+
 compile_postgres() {
     # check if the tag exist or else stop the program
-    find_if_tag_exist
     
     cd $postgres_source_directory
 
@@ -90,19 +106,19 @@ compile_postgres() {
     fi
 }
 
-set_replicas() {
+must_have_binary() {
+    if [ -f $1 ];then
+        echo "[ERROR] The file $1 is not found"
+        exit 1
+    fi
+}
+
+set_master_server() {
 
     if [ -d $masterDirectory ];then
         rm -r $masterDirectory
     fi
-
-
-    initdb=$buildDirectory/$version/bin/initdb
-    pg_ctl=$buildDirectory/$version/bin/pg_ctl
-    psql=$buildDirectory/$version/bin/psql
-    pg_backup=$buildDirectory/$version/bin/pg_basebackup
-
-    # hba_conf
+     # hba_conf
     hba_confLine="host  replication repuser   127.0.0.1/32     trust"
     hba_conf_file=$masterDirectory/pg_hba.conf
 
@@ -119,7 +135,8 @@ set_replicas() {
         echo "Starting of master server failed"
         exit 1
     fi
-echo "[INFO] Creating a replication user"
+    
+    echo "[INFO] Creating a replication user"
     if ! $psql --port=$masterPort -c "create user repuser replication" postgres >> replica.log 2>&1;then
         echo "Couldn't create user repuser"
         exit 1
@@ -136,27 +153,61 @@ echo "[INFO] Creating a replication user"
 
     echo "[INFO] Master server running in port $masterPort"
 
+}
+
+set_replicas() {
+    
     echo "[INFO] Configuring standby server"
 
     for ((i=1 ; i<=$numberOfReplicas;i++));do
+        
         slotName="rep_slot_$i"
         slaveDirectory="${baseReplicaDirectoryString}$i"
+        
         if [ -d $slaveDirectory ];then
             rm -rf $slaveDirectory;
         fi
+
+        if lsof -i ":$slavePort";then
+            echo "[WARN] Port is not available switching to next port"
+            ((slavePort++))
+            ((i--))
+            continue;
+        fi
+
         if ! $pg_backup -h localhost -U repuser --checkpoint=fast -D $slaveDirectory -R --slot=$slotName -C --port=$masterPort >> replica.log 2>&1;then
-            echo "Configuring Standby server for dir : $slaveDirectory , port : $slavePort failed"
-            exit 1;
+            echo "[ERROR] Configuring Standby server for dir : $slaveDirectory , port : $slavePort failed"
+            continue
         fi
 
         if ! $pg_ctl -D $slaveDirectory -o "-p $slavePort" start >> replica.log;then
-            echo "Starting the standby server for dir :$slaveDirectory , port : $slavePort failed"
-            exit 1;
+            echo "[ERROR] Starting the standby server for dir :$slaveDirectory , port : $slavePort failed"
+            continue
         fi
         ((slavePort++))
     done
     echo "[SUCCESS] Successfully configured the server for Replicas $numberOfReplicas"
 }
 
+
+# if no postgres found clone it
+# and if the master dir already exist remove it
+setup_environment
+
+# check whether the inputs are valid or not
+check_input
+
+# if valid compile the postgres server
 compile_postgres
+
+# checks if the binaries of these exists or not
+must_have_binary $initdb
+must_have_binary $pg_ctl
+must_have_binary $psql
+must_have_binary $pg_backup
+
+# create a master instance of the server
+set_master_server
+
+# set the replicas based on the given input
 set_replicas
