391a78da173661455c4aff1c1979e4834f873537
darshan744 <darshankumaravelk@gmail.com>
2025-09-16 12:32:00 +0530

    Readme file

--- Task-3/create_server.sh
+++ Task-3/create_server.sh
@@ -8,6 +8,7 @@ distCleanLogs=$(pwd)/distclean.log
 configLog=$(pwd)/configure.log
 makeLog=$(pwd)/make.log
 makeInstallLog=$(pwd)/makeinstall.log
+replicalog=$(pwd)/replica.log
 
 # Master and Replica Directories and port
 masterDirectory=$HOME/Documents/Auto/Node
@@ -24,7 +25,6 @@ initdb=$buildDirectory/$version/bin/initdb
 pg_ctl=$buildDirectory/$version/bin/pg_ctl
 psql=$buildDirectory/$version/bin/psql
 pg_backup=$buildDirectory/$version/bin/pg_basebackup
-
 
 
 # if source not found clone it
@@ -89,7 +89,7 @@ compile_postgres() {
     if ! make distclean > $distCleanLogs 2>&1;then
         echo "Theres nothing to clean"
     fi
-
+    
     echo "[INFO] Running ./configure please watch logs at $configLog"
     if ! ./configure --prefix="$buildDirectory/$version" > $configLog 2>&1;then
         echo "[ERROR] Configuring the postgres source failed please verify the logs"
@@ -127,20 +127,20 @@ set_master_server() {
 
     echo "[INFO] Initializing Master Directory"
     
-    if ! $initdb -D $masterDirectory> replica.log 2>&1;then
+    if ! $initdb -D $masterDirectory > $replicalog 2>&1;then
         echo "[ERROR] Couldn't initialize the Master database"
         exit 1
     fi
 
     echo "[INFO] Starting master server"
 
-    if ! $pg_ctl -D $masterDirectory -o "-p $masterPort" start >> replica.log 2>&1;then
+    if ! $pg_ctl -D $masterDirectory -o "-p $masterPort" start >> $replicalog 2>&1;then
         echo "Starting of master server failed"
         exit 1
     fi
     
     echo "[INFO] Creating a replication user"
-    if ! $psql --port=$masterPort -c "create user repuser replication" postgres >> replica.log 2>&1;then
+    if ! $psql --port=$masterPort -c "create user repuser replication" postgres >> $replicalog 2>&1;then
         echo "Couldn't create user repuser"
         exit 1
     fi
@@ -149,7 +149,7 @@ set_master_server() {
 
     echo "[INFO] Restarting the master server"
 
-    if ! $pg_ctl -D $masterDirectory reload >> replica.log 2>&1;then
+    if ! $pg_ctl -D $masterDirectory reload >> $replicalog 2>&1;then
         echo "Restarting the master server failed"
         exit 1
     fi
@@ -178,12 +178,12 @@ set_replicas() {
             continue;
         fi
 
-        if ! $pg_backup -h localhost -U repuser --checkpoint=fast -D $slaveDirectory -R --slot=$slotName -C --port=$masterPort >> replica.log 2>&1;then
+        if ! $pg_backup -h localhost -U repuser --checkpoint=fast -D $slaveDirectory -R --slot=$slotName -C --port=$masterPort >> $replicalog 2>&1;then
             echo "[ERROR] Configuring Standby server for dir : $slaveDirectory , port : $slavePort failed"
             continue
         fi
 
-        if ! $pg_ctl -D $slaveDirectory -o "-p $slavePort" start >> replica.log;then
+        if ! $pg_ctl -D $slaveDirectory -o "-p $slavePort" start >> $replicalog;then
             echo "[ERROR] Starting the standby server for dir :$slaveDirectory , port : $slavePort failed"
             continue
         fi
@@ -191,7 +191,6 @@ set_replicas() {
     done
     echo "[SUCCESS] Successfully configured the server for Replicas $numberOfReplicas"
 }
-
 
 # if no postgres found clone it
 # and if the master dir already exist remove it
